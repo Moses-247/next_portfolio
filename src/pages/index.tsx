4ba386f4b1f270371c1fd063c3310ece532091c6
Moses-247 <asugam9@gmail.com>
2025-02-14 01:06:30 +0300

    next

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import styles from '../styles/Home.module.css';
+import Image from 'next/image';
+import profilePic from '../public/profile-pic.jpg';
+
+const Home: React.FC = () => {
+  return (
+    <div className={styles.container}>
+        <Image src={profilePic} alt="Profile Picture" width={150} height={150} />
+      <h1 className={styles.heading}>Welcome to My Portfolio</h1>
+      {/* Add more content here */}
+    </div>
+  );
+};
+
+
+export default Home;
---
+++ src/pages/projects.tsx
@@ -0,0 +1,23 @@
+// pages/projects.tsx
+export const getServerSideProps = async () => {
+    const res = await fetch('https://api.example.com/projects');
+    const projects = await res.json();
+  
+    return { props: { projects } };
+  };
+  
+  const Projects: React.FC<{ projects: any[] }> = ({ projects }) => {
+    return (
+      <div>
+        <h1>Projects</h1>
+        <ul>
+          {projects.map((project) => (
+            <li key={project.id}>{project.name}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  };
+  
+  export default Projects;
+  
